cd9108024d52cb427f1673e27f67526277f2cd6a
Eirvane <Eirvane@foxmail.com>
2026-09-09 19:07:28 +0800

    🔨Fat: Integrate system tray icon with context menu support

--- src/platform/trayicon.c
+++ src/platform/trayicon.c
@@ -1,15 +1,14 @@
 ﻿#include "trayicon.h"
 #include "window.h"   /* 使用 g_hInstance, g_hClockWnd, WM_TRAYICON, ID_TRAY_ICON */
+#include "../../resource.h"
 #include <shellapi.h>
 
 #pragma comment(lib, "shell32.lib")
 
-/* 托盘图标数据结构 */
 static NOTIFYICONDATAW g_nid = { 0 };
 
 void TrayIcon_Init(HWND hWnd, HINSTANCE hInstance)
 {
-    /* 同步全局状态（供其他模块使用） */
     g_hInstance = hInstance;
     g_hClockWnd = hWnd;
 
@@ -19,7 +18,8 @@ void TrayIcon_Init(HWND hWnd, HINSTANCE hInstance)
     g_nid.uID = ID_TRAY_ICON;
     g_nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
     g_nid.uCallbackMessage = WM_TRAYICON;
-    g_nid.hIcon = LoadIconW(NULL, IDI_APPLICATION);
+    g_nid.hIcon = (HICON)LoadImageW(hInstance, MAKEINTRESOURCEW(IDI_APP_ICON),
+        IMAGE_ICON, GetSystemMetrics(SM_CXSMICON), GetSystemMetrics(SM_CYSMICON), LR_DEFAULTSIZE);
     wcscpy_s(g_nid.szTip, 128, L"桌面时钟");
 
     Shell_NotifyIconW(NIM_ADD, &g_nid);
--- src/platform/trayicon.h
+++ src/platform/trayicon.h
@@ -3,10 +3,7 @@
 
 #include <windows.h>
 
-/* 初始化系统托盘图标 */
 void TrayIcon_Init(HWND hWnd, HINSTANCE hInstance);
-
-/* 移除系统托盘图标（应在程序退出前调用） */
 void TrayIcon_Remove(HWND hWnd);
 
 #endif /* TRAYICON_H */
